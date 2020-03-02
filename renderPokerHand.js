1504f5f76b5cd66b89161ff08d225701b4d7c2cd
Abdella <abdella.shukur2@gmail.com>
2020-03-02 02:09:49 -0500

    updated

--- renderPokerHand.js
+++ renderPokerHand.js
@@ -1,39 +1,40 @@
 
-function renderPokerHand(pokerHand) {
-    // HINT: You can use <img /> tags that point to the card images in the /cards folder
-    return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(pokerHand)}</code>
-        </div>
-    `
-}
+function renderPokerHand() {
+    // HINT: You can use <img /> tags that point to the card images in the /cards folder 
+    for (i=0; i < 5;i++){
+     return `<div class="text-center mt-5">
+        <img src="cards/${pokerHandAbs[i].value}${pokerHandAbs[i].suit}.png" width="100"/>
+        </div>`
+       }
+    }
+var pokerHandAbs = [
+    {
+        value: "K",
+        suit: "C"
+    },
+    {
+        value: "K",
+        suit: "D"
+    },
+    {
+        value: "9",
+        suit: "S"
+    },
+    {
+        value: "2",
+        suit: "H"
+    },
+    {
+        value: "9",
+        suit: "H"
+    }
+]
 
 function pokerHand() {
     var content = document.getElementById('content');
 
-    var pokerHandAbstraction = [
-        {
-            value: "K",
-            suit: "C"
-        },
-        {
-            value: "K",
-            suit: "D"
-        },
-        {
-            value: "9",
-            suit: "S"
-        },
-        {
-            value: "2",
-            suit: "H"
-        },
-        {
-            value: "9",
-            suit: "H"
-        }
-    ];
+    
 
-    content.innerHTML = renderPokerHand(pokerHandAbstraction);
+    content.innerHTML = renderPokerHand(pokerHandAbs);
 
 }
