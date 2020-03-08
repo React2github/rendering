f2d9fb34d427e7686ba60cb39b0adce09370c183
Abdella <abdella.shukur2@gmail.com>
2020-03-08 13:42:14 -0400

    names

--- renderNametags.js
+++ renderNametags.js
@@ -1,14 +1,12 @@
+function renderNametags(nametags) {
 
-function renderNametags(nameTags, names) {
-    var names = '';
-    var i;
-    for (i=0;i<nameTags.length;i++) {
-        names += nameTags[i]+"<br>";
-    }
-    return `
-        <div class="text-center mt-5">
-        <div>${names}</div>
-    `
+    var nameDivs = nametags.map(e => {
+        return `
+            <div>${e}</div>
+        `
+    });
+    
+    return nameDivs.join('');
 }
 
 function nametags() {
